c582b7000547010521656f4b2ac9a51d04269d3f
Oliver Tzeng <olivertzeng@proton.me>
2025-12-26 02:31:36 +0800

    watcher

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -526,8 +526,19 @@ require("lazy").setup({
 			-- end,
 		},
 	},
+	-- lazy.nvim
 	{
-		"ActivityWatch/aw-watcher-vim",
+		"lowitea/aw-watcher.nvim",
+		opts = { -- required, but can be empty table: {}
+			-- add any options here
+			-- for example:
+			aw_server = {
+				host = "127.0.0.1",
+				port = 5600,
+			},
+		},
+	},
+	{
 		"Bekaboo/dropbar.nvim",
 		"OXY2DEV/helpview.nvim",
 		"dstein64/nvim-scrollview",
