a80fe8eb669a73e91415e39e522101d132f25eeb
Oliver Tzeng <olivertzeng@proton.me>
2026-02-22 21:58:50 +0800

    new stuff

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,17 +1,17 @@
 require("lazy").setup({
-	{
-		"OXY2DEV/markview.nvim",
-		config = function(plugin, opts)
-			-- Force markview.nvim to be sourced before everything else
-			vim.opt.rtp:prepend(plugin.dir)
-
-			-- Set up the plugin
-			require("markview").setup(opts)
-		end,
-		event = "InsertEnter",
-		ft = { "markdown", "html", "tex", "plaintex", "typst", "yaml" },
-		lazy = false,
-	},
+	-- {
+	-- 	"OXY2DEV/markview.nvim",
+	-- 	config = function(plugin, opts)
+	-- 		-- Force markview.nvim to be sourced before everything else
+	-- 		vim.opt.rtp:prepend(plugin.dir)
+	--
+	-- 		-- Set up the plugin
+	-- 		require("markview").setup(opts)
+	-- 	end,
+	-- 	event = "InsertEnter",
+	-- 	ft = { "markdown", "html", "tex", "plaintex", "typst", "yaml" },
+	-- 	lazy = false,
+	-- },
 	{
 		"goolord/alpha-nvim",
 		config = function()
@@ -243,6 +243,7 @@ require("lazy").setup({
 					"bash",
 					"bibtex",
 					"c",
+					"caddy",
 					"cpp",
 					"css",
 					"csv",
